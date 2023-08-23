a434eb095c1ba55241fd78a9c756db6a4311ae24
Prabhash Kumar Jha <76786287+prabhashj07@users.noreply.github.com>
2023-08-23 17:47:41 +0545

    Update scripts.js

--- template/scripts.js
+++ template/scripts.js
@@ -1,16 +1,26 @@
-// scripts.js
-const form = document.getElementById('upload-form');
-const resultDiv = document.getElementById('conversion-result');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('upload-form');
+    const resultDiv = document.getElementById('conversion-result');
 
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-    const formData = new FormData(form);
-    const response = await fetch('/upload', {
-        method: 'POST',
-        body: formData,
+        const formData = new FormData(form);
+        try {
+            const response = await fetch('/upload', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (!response.ok) {
+                throw new Error('Conversion request failed');
+            }
+
+            const resultText = await response.text();
+            resultDiv.innerHTML = `<h2>Conversion Result:</h2><pre>${resultText}</pre>`;
+        } catch (error) {
+            resultDiv.innerHTML = '<p>An error occurred during conversion. Please try again later.</p>';
+            console.error(error);
+        }
     });
-
-    const resultText = await response.text();
-    resultDiv.innerHTML = `<h2>Conversion Result:</h2><pre>${resultText}</pre>`;
 });
